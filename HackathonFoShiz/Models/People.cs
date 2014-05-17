52bb5a3e97de6143748fbb973cf7c0b4c77150c5
Troy Whorten <troy.whorten@wyo.gov>
2014-05-17 13:22:07 -0600

    Added initial classes and dbcontext

---
+++ HackathonFoShiz/Models/People.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackathonFoShiz.Models
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CellPhone { get; set; }
+        public string OtherPhone { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ HackathonFoShiz/Models/PeopleLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackathonFoShiz.Models
+{
+    public class PeopleLocation
+    {
+        public int Id { get; set; }
+        public int PeopleId { get; set; }
+        public int LocationId { get; set; }
+    }
+}
