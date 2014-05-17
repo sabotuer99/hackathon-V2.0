91155d01394ffcd33b31adebcf291c081a47cfac
bjorn22 <tyler.bjornestad@wyo.gov>
2014-05-17 13:27:33 -0600

    Added angular

---
+++ HackathonFoShiz/Scripts/app/app.js
@@ -0,0 +1,23 @@
+﻿var mainApp = angular.module('mainApp', ['ngRoute', 'ngAnimate', 'locationServices', 'locationControllers']);
+
+mainApp.config([
+    '$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/location', {
+                templateUrl: '/Partials/Location.html',
+                controller: 'locationControllers'
+            }).
+            //when('/cards/:cardId', {
+            //    templateUrl: '/Scripts/app/Views/detail.html',
+            //    controller: 'cardDetailController'
+            //}).
+            //when('/cars', {
+            //    templateUrl: '/Partials/carList.html',
+            //    controller: 'carController'
+            //}).
+            otherwise({
+                redirectTo: '/location'
+            });
+    }
+]);
---
+++ HackathonFoShiz/Scripts/app/services/LocationServices.js
@@ -0,0 +1,23 @@
+﻿var locationServices = angular.module('locationServices', ['ngResource']);
+
+var url = '/Api/Location';
+locationServices.factory('locationFactory', function ($http) {
+    return {
+        list: function () {
+            return $http.get(url);
+        },
+        get: function (id) {
+            return $http.get(url, id);
+        },
+        put: function (location) {
+            return $http.post(url, location);
+        },
+        del: function (location) {
+            return $http.delete(url + location.id);
+        },
+        update: function (location) {
+            return $http.put(url + location.Id, location);
+        }
+
+    };
+});
