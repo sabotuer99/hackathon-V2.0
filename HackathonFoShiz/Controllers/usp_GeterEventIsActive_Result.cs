d16c9b429bc9fd327932b71f1b0ab8aa190dbfe9
JRNunamaker <fred.nunamaker@wyo.gov>
2014-05-18 07:44:59 -0600

    Disregard

---
+++ HackathonFoShiz/Controllers/usp_GeterEventIsActive_Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HackathonFoShiz.Controllers
+{
+    public partial class usp_GeterEventIsActive_Result
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public System.DateTime BeginDate { get; set; }
+        public System.DateTime EndDate { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
